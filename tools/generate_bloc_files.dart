35e6d71082041d37a9a1fc0264397bda9285bdd8
Lệ Ngư Hà <duongthecuongbk227@gmail.com>
2025-02-10 20:54:05 +0700

    Update generate_bloc_files.dart

--- tools/generate_bloc_files.dart
+++ tools/generate_bloc_files.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
     print('Usage: dart generate_bloc_files.dart <ScreenName>');
@@ -7,7 +8,16 @@ void main(List<String> arguments) {
   }
 
   final screenName = arguments[0];
+  final folderName = screenName.toLowerCase();
+
   final screenNameLower = screenName.toLowerCase();
+  var className = screenName;
+  if (screenName.contains('_')) {
+    className = capitalizeAfterUnderscore(screenName);
+  } else {
+    className = capitalizeFirstLetter(screenName);
+  }
+
   final templates = {
     'bloc': 'tools/templates/template_bloc.txt',
     'state': 'tools/templates/template_state.txt',
@@ -17,7 +27,7 @@ void main(List<String> arguments) {
     'index': 'tools/templates/index.txt',
   };
 
-  final outputDir = 'lib/presentation/page/$screenNameLower';
+  final outputDir = 'lib/presentation/page/$folderName';
 
   // Create the directory if it doesn't exist
   final directory = Directory(outputDir);
@@ -29,23 +39,46 @@ void main(List<String> arguments) {
     final templateFile = File(entry.value);
     if (!templateFile.existsSync()) {
       print('Template file ${entry.value} does not exist');
+
       return;
     }
 
     final templateContent = templateFile.readAsStringSync();
     final newContent = templateContent
-        .replaceAll('{ScreenName}', screenName)
-        .replaceAll('{screen_name}', screenNameLower);
+        .replaceAll('{ScreenName}', className)
+        .replaceAll('{screen_name}', className);
     final newFile = File('$outputDir/${screenNameLower}_${entry.key}.dart');
 
     if (entry.key == 'index') {
+
+      final newContentIndex = templateContent
+          .replaceAll('{ScreenName}', screenName)
+          .replaceAll('{screen_name}', screenNameLower);
+
       final newIndexFile = File('$outputDir/index.dart');
-      newIndexFile.writeAsStringSync(newContent);
+      newIndexFile.writeAsStringSync(newContentIndex);
       print('File $outputDir/index.dart created successfully.');
+
     } else {
       newFile.writeAsStringSync(newContent);
       print('File $outputDir/${screenNameLower}_${entry.key}.dart created successfully.');
+
     }
 
   }
+}
+
+
+String capitalizeAfterUnderscore(String input) {
+  return input.split('_').map((word) {
+    if (word.isNotEmpty) {
+      return word[0].toUpperCase() + word.substring(1);
+    }
+    return word;
+  }).join('');
+}
+
+String capitalizeFirstLetter(String word) {
+  if (word.isEmpty) return word;
+  return word[0].toUpperCase() + word.substring(1);
 }
